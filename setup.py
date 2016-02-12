720773ea311abe01be83982f26a61ef744f9f648
Kyle Nickel <nickelkr@gmail.com>
2016-02-11 20:56:48 -0500

    Setup changes

--- setup.py
+++ setup.py
@@ -3,11 +3,13 @@ from distutils.core import setup
 setup(
     name='yfi',
     packages=['yfi'],
-    version='0.0.1',
-    description='Yahoo YQL Library',
+    version='0.0.2',
+    license='MIT',
+    summary='Library for Yahoo YQL',
+    description="YFi allows you to create and run queries against Yahoo's YQL datatables",
     author='Kyle Nickel',
     url='https://github.com/nickelkr/yfi',
-    download_url='https://github.com/nickelkr/yfi/tarball/0.0.1',
+    download_url='https://github.com/nickelkr/yfi/tarball/0.0.2',
     keywords=['Yahoo', 'YQL', 'finance', 'data', 'open'],
     classifiers=[],
 )
